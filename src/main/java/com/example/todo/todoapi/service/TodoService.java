0a45dbed25fc9ab63b40a2d81ddd0c9adc8209d1
itacademy <9oo9leplex@gamil.com>
2023-02-01 10:23:22 +0900

    bootJar

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,131 +1,93 @@
 package com.example.todo.todoapi.service;
 
-import com.example.todo.todoapi.dto.rq.TodoCreateRqDTO;
-import com.example.todo.todoapi.dto.rq.TodoModifyRqDTO;
-import com.example.todo.todoapi.dto.rs.TodoDetailRsDTO;
-import com.example.todo.todoapi.dto.rs.TodoListRsDTO;
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import com.example.todo.todoapi.entity.TodoEntity;
 import com.example.todo.todoapi.repository.TodoRepository;
-import jdk.jshell.spi.ExecutionControl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class TodoService {
 
     private final TodoRepository todoRepository;
 
-    public TodoDetailRsDTO findById(String todoId){
-
-        Optional<TodoEntity> targetEntity = todoRepository.findById(todoId);
-
-        return targetEntity
-                .map(TodoDetailRsDTO::new).orElseThrow();
-    }
 
     // 할 일 목록 조회
-    public TodoListRsDTO retrieve(){
+    @Transactional
+    public TodoListResponseDTO retrieve(String userId) {
+        List<TodoEntity> entityList = todoRepository.findByUserId(userId);
 
-        List<TodoEntity> entityList = todoRepository.findAll();
-
-        List<TodoDetailRsDTO> todoList = entityList.stream()
-                .map(te -> new TodoDetailRsDTO(te))
+        List<TodoDetailResponseDTO> dtoList = entityList.stream()
+                .map(TodoDetailResponseDTO::new)
                 .collect(Collectors.toList());
 
-        return TodoListRsDTO.builder()
-                .todos(todoList)
+        return TodoListResponseDTO.builder()
+                .todos(dtoList)
                 .build();
     }
 
     // 할 일 등록
-    public TodoListRsDTO create(final TodoCreateRqDTO createRqDTO) throws ExecutionControl.RunException {
+    public TodoListResponseDTO create(
+            final TodoCreateRequestDTO createRequestDTO
+            , final String userId
+    )
+        throws RuntimeException
+    {
 
+        TodoEntity todo = createRequestDTO.toEntity();
+        todo.setUserId(userId);
 
-        todoRepository.save(createRqDTO.todoEntity());
-        log.info("할 일이 저장되었습니다. 제목 : {}", createRqDTO.getTitle());
-
-        return retrieve();
+        todoRepository.save(todo);
+        log.info("할 일이 저장되었습니다. 제목 : {}", createRequestDTO.getTitle());
+        return retrieve(userId);
     }
-    
-    // 할 일 수정
-    public TodoListRsDTO update(final String id, final TodoModifyRqDTO todoModifyRqDTO){
 
+    // 할 일 수정 (제목, 할일 완료여부)
+    public TodoListResponseDTO update(
+            final String id,
+            final TodoModifyRequestDTO modifyRequestDTO,
+            final String userId
+    ) {
         Optional<TodoEntity> targetEntity = todoRepository.findById(id);
 
         targetEntity.ifPresent(entity -> {
-            entity.setTitle(todoModifyRqDTO.getTitle());
-            entity.setDone(todoModifyRqDTO.isDone());
+            entity.setTitle(modifyRequestDTO.getTitle());
+            entity.setDone(modifyRequestDTO.isDone());
 
             todoRepository.save(entity);
         });
 
-        return retrieve();
+        return retrieve(userId);
     }
 
     // 할 일 삭제
-    public TodoListRsDTO delete(final String id){
+    public TodoListResponseDTO delete(final String id, final String userId) {
 
 
         try {
             todoRepository.deleteById(id);
-
-            return retrieve();
         } catch (Exception e) {
-            log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}, error: {}",id,e.getMessage());
+            log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}, err: {}"
+                                    , id, e.getMessage());
             throw new RuntimeException("id가 존재하지 않아 삭제에 실패했습니다.");
         }
+        return retrieve(userId);
     }
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src/test/java/com/example/todo/todoapi/service/TodoServiceTest.java
+++ src/test/java/com/example/todo/todoapi/service/TodoServiceTest.java
@@ -1,10 +1,9 @@
 package com.example.todo.todoapi.service;
 
-import com.example.todo.todoapi.dto.rq.TodoCreateRqDTO;
-import com.example.todo.todoapi.dto.rq.TodoModifyRqDTO;
-import com.example.todo.todoapi.dto.rs.TodoDetailRsDTO;
-import com.example.todo.todoapi.dto.rs.TodoListRsDTO;
-import com.example.todo.todoapi.repository.TodoRepository;
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +14,7 @@ import org.springframework.test.annotation.Commit;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Commit
 class TodoServiceTest {
@@ -23,79 +23,60 @@ class TodoServiceTest {
     TodoService todoService;
 
     @BeforeEach
-    void beforeInsert(){
+    void beforeInsert() {
+        TodoCreateRequestDTO dto1 = TodoCreateRequestDTO.builder().title("저녁 장보기").build();
+        TodoCreateRequestDTO dto2 = TodoCreateRequestDTO.builder().title("식물 물주기").build();
+        TodoCreateRequestDTO dto3 = TodoCreateRequestDTO.builder().title("음악감상하기").build();
 
-        TodoCreateRqDTO dto1 = TodoCreateRqDTO.builder().title("저녁 장보기").build();
-        TodoCreateRqDTO dto2 = TodoCreateRqDTO.builder().title("식물 물주기").build();
-        TodoCreateRqDTO dto3 = TodoCreateRqDTO.builder().title("음악 감상하기").build();
-
-        todoService.create(dto1);
-        todoService.create(dto2);
-        todoService.create(dto3);
+//        todoService.create(dto1);
+//        todoService.create(dto2);
+//        todoService.create(dto3);
     }
 
     @Test
     @DisplayName("새로운 할 일을 등록하면 생성되는 리스트는 할 일이 4개 들어있어야 한다.")
-    void createTest(){
+    void createTest() {
         // given
-        TodoCreateRqDTO newTodo = TodoCreateRqDTO.builder()
-                .title("새로운 할일~~")
-                .build();
-        // when
-        TodoListRsDTO rsDTO = todoService.create(newTodo);
-
-        // then
-        List<TodoDetailRsDTO> todos = rsDTO.getTodos();
-        assertEquals(4, todos.size());
-
-        todos.forEach(System.out::println);
+//        TodoCreateRequestDTO newTodo = TodoCreateRequestDTO.builder()
+//                .title("새로운 할일~~")
+//                .build();
+//        // when
+//        TodoListResponseDTO responseDTO = todoService.create(newTodo);
+//
+//        // then
+//        List<TodoDetailResponseDTO> todos = responseDTO.getTodos();
+//        assertEquals(4, todos.size());
+//
+//        System.out.println("=====================================");
+//        todos.forEach(System.out::println);
     }
 
     @Test
-    @DisplayName("2번째 할 일의 제목을 수정수정으로 수종하고 할 일 완료처리를 해야한다.")
-    void updateTest(){
+    @DisplayName("2번째 할일의 제목을 수정수정으로 수정하고 할일 완료처리를 해야한다.")
+    void updateTest() {
         // given
-        String newTitle = "수정수정";
-        boolean newDone = true;
-
-        TodoModifyRqDTO modifyRqDTO = TodoModifyRqDTO.builder()
-                .title(newTitle)
-                .isDone(newDone)
-                .build();
-        // when
-        TodoDetailRsDTO targetTodo =
-                todoService.retrieve().getTodos().get(1);
-
-        TodoListRsDTO rsDTO = todoService.update(targetTodo.getId(), modifyRqDTO);
-
-        // then
-        assertEquals("수정수정", rsDTO.getTodos().get(1).getTitle());
-        assertTrue(rsDTO.getTodos().get(1).isDone());
+//        String newTitle = "수정수정";
+//        boolean newDone = true;
+//
+//        TodoModifyRequestDTO modifyRequestDTO
+//                = TodoModifyRequestDTO.builder()
+//                .title(newTitle)
+//                .done(newDone)
+//                .build();
+//
+//        // when
+//        TodoDetailResponseDTO targetTodo
+//                = todoService.retrieve().getTodos().get(1);
+//
+//        TodoListResponseDTO responseDTO
+//                = todoService.update(targetTodo.getId(), modifyRequestDTO);
+//
+//        // then
+//        assertEquals("수정수정", responseDTO.getTodos().get(1).getTitle());
+//        assertTrue(responseDTO.getTodos().get(1).isDone());
+//
+//        System.out.println("===========================================");
+//        responseDTO.getTodos().forEach(System.out::println);
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
